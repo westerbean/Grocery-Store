4a7cb6634ad216ba71073757076e621b6a66b3f7
Femi Aderewa <136677969+westerbean@users.noreply.github.com>
2024-07-21 01:20:35 +0100

    Update sql_connection.py

--- Grocery_Application/Backend/sql_connection.py
+++ Grocery_Application/Backend/sql_connection.py
@@ -5,7 +5,7 @@ __cnx = None
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='Brbquick1',
+        __cnx = mysql.connector.connect(user='root', password='password',
                                           host='127.0.0.1',
                                           database='grocery_store')
     return __cnx
